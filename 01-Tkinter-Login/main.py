e83ed6f1a4c234e155b1a4b4917618f5444882cc
Joel Alejandro Espinoza Sánchez <alex.esa18@gmail.com>
2023-11-02 21:09:11 -0600

    Finished first video project

---
+++ 01-Tkinter-Login/main.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from tkinter import messagebox
+
+window = tk.Tk()
+window.title("Login form")
+window.geometry('340x440')
+window.configure(bg = '#333333')
+
+def login():
+    username = "user"
+    password = "pass"
+    if (username_entry.get() == username) and (password_entry.get() == password):
+        messagebox.showinfo(title = "Login success", message = "You successfully logged in")
+    else:
+        messagebox.showinfo(title = "Invalid login", message = "Invalid log in")
+
+frame = tk.Frame(bg = '#333333')
+
+# Creating widgets
+login_label = tk.Label(frame, text = "Login", bg = "#333333", fg = "#FF3399", font = ("Arial", 30))
+username_label = tk.Label(frame, text = "Username", bg = "#333333", fg = "#FFFFFF", font = ("Arial", 16))
+username_entry = tk.Entry(frame, font = ("Arial", 16))
+password_label = tk.Label(frame, text = "Password", bg = "#333333", fg = "#FFFFFF", font = ("Arial", 16))
+password_entry = tk.Entry(frame, show = "*", font = ("Arial", 16))
+login_button = tk.Button(frame, text = "Login", bg = "#FF3399", fg = "#FFFFFF", font = ("Arial", 16), command = login)
+
+# Placing widgets on window
+login_label.grid(row = 0, column = 0, columnspan = 2, sticky = "news", pady = 40)
+username_label.grid(row = 1, column = 0)
+username_entry.grid(row = 1, column = 1, pady = 20)
+password_label.grid(row = 2, column = 0)
+password_entry.grid(row = 2, column = 1, pady = 20)
+login_button.grid(row = 3, column = 0, columnspan = 2, pady = 30)
+
+frame.pack()
+
+window.mainloop()
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # CFWH-TkinterProjects
-Repo for learning GUI with tkinter using Code First with Hala YouTube playlist
+
+Repository made for learning GUI with tkinter based on the YouTube playlist [Tkinter GUI Projects](https://www.youtube.com/playlist?list=PLs3IFJPw3G9IiHm9PEP1UaMtuvACmxVMj) by [Code First with Hala](https://www.youtube.com/@codefirstwithhala) with the following videos:
+
+1. [Tkinter Login Form tutorial for COMPLETE beginners [responsive layout]](https://www.youtube.com/watch?v=MeMCBdnhvQs)
+2. []()
+3. []()
+4. []()
+5. []()
+6. []()
+7. []()
+8. []()
+9. []()
+10. []()
+11. []()
+12. []()
+13. []()
+14. []()
+15. []()
